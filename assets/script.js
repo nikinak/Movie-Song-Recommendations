aa91e851221c055a0466a91db94cbe3de5a698c4
Alex Hsieh <alexhsieh12@gmail.com>
2022-09-26 20:37:32 -0700

    psuh

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,22 @@
-const dropdown = document.getElementById('')
+const dropdown = document.getElementById('dropdownchoices')
+var displayContainer3 = document.getElementById('displaycontainer3')
+var dropdownhide = document.getElementById('displaycontainer2')
+var morbius = document.getElementById('morbius')
+
+start()
+
+
+function start() {
+    dropdownhide.classList.remove('hide')
+    dropdown.addEventListener('click', funcContainer3)
+}
+
+function funcContainer3() {
+    displayContainer3.classList.remove('hide')
+    dropdownhide.classList.add('hide')
+    showArtists()
+}
+
+function showArtists(Artists) {
+
+}
